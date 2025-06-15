3a4d1da7fb547c013375a46a3216280a25cb1bee
ValeryTychkin <valery.tychkin@gmail.com>
2025-06-15 19:47:06 +0300

    Add validator string_1_1024 and string_0_1024

--- crates/libs/util/src/string.rs
+++ crates/libs/util/src/string.rs
@@ -47,6 +47,14 @@ pub mod validate {
         v.len() <= 255
     }
 
+    pub fn is_str_1_1024(v: &str) -> bool {
+        0 < v.len() && v.len() <= 1024
+    }
+
+    pub fn is_str_0_1024(v: &str) -> bool {
+        v.len() <= 1024
+    }
+
     pub fn is_str_1_2048(v: &str) -> bool {
         0 < v.len() && v.len() <= 2048
     }
@@ -120,6 +128,78 @@ pub mod validate {
             Ok(v) => {
                 if !validate::is_str_0_255(v.as_str()) {
                     return Err(D::Error::custom("invalid length, String must be lte 255"));
+                }
+                Ok(v)
+            }
+            Err(e) => Err(e),
+        }
+    }
+
+    /// Validates that a deserialized string has a length between 1 and 1024 characters.
+    ///
+    /// This function uses the `validate::is_str_1_1024` utility to enforce the constraint.
+    /// If the string length is not within the range, it returns a custom deserialization error.
+    ///
+    /// # Parameters
+    /// - `d`: A deserializer that provides the input to be deserialized into a String.
+    ///
+    /// # Returns
+    /// - `Ok(String)` if the string satisfies the length constraint.
+    /// - `Err(D::Error)` if the string is invalid or the deserialization fails.
+    ///
+    /// # Example
+    /// ```
+    /// #[derive(Deserialize)]
+    /// struct MyStruct {
+    ///     #[serde(deserialize_with = "string_1_1024")]
+    ///     name: String,
+    /// }
+    /// ```
+    pub fn string_1_1024<'de, D>(d: D) -> Result<String, D::Error>
+    where
+        D: Deserializer<'de>,
+    {
+        match String::deserialize(d) {
+            Ok(v) => {
+                if !validate::is_str_1_1024(v.as_str()) {
+                    return Err(D::Error::custom(
+                        "invalid length, String must be lte 1024 and gt 0",
+                    ));
+                }
+                Ok(v)
+            }
+            Err(e) => Err(e),
+        }
+    }
+
+    /// Validates that a deserialized string has a length between 0 and 1024 characters.
+    ///
+    /// This function uses the `validate::is_str_0_1024` utility to enforce the constraint.
+    /// If the string length is not within the range, it returns a custom deserialization error.
+    ///
+    /// # Parameters
+    /// - `d`: A deserializer that provides the input to be deserialized into a String.
+    ///
+    /// # Returns
+    /// - `Ok(String)` if the string satisfies the length constraint.
+    /// - `Err(D::Error)` if the string is invalid or the deserialization fails.
+    ///
+    /// # Example
+    /// ```
+    /// #[derive(Deserialize)]
+    /// struct MyStruct {
+    ///     #[serde(deserialize_with = "string_0_1024")]
+    ///     name: String,
+    /// }
+    /// ```
+    pub fn string_0_1024<'de, D>(d: D) -> Result<String, D::Error>
+    where
+        D: Deserializer<'de>,
+    {
+        match String::deserialize(d) {
+            Ok(v) => {
+                if !validate::is_str_1_1024(v.as_str()) {
+                    return Err(D::Error::custom("invalid length, String must be lte 1024"));
                 }
                 Ok(v)
             }
